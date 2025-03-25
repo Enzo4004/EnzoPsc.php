26db9ad11386f024a04cc1d90b5ee5a02a0996ad
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-03-25 14:31:52 +0100

    les base du projet

---
+++ controllers/create.order.controllers.php
@@ -0,0 +1,5 @@
+<?php 
+//ca fait appelle a la pages view si besoin 
+require_once("../create.order.view.php");
+//ca fait appelle a la page pour afficher les erreurs 
+require_once("../config.php") ;
