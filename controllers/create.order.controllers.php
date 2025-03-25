caa13185a8658429cb2c9fedb8f48c9a81cd0707
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-03-25 15:38:44 +0100

    Modification du champs Select

--- controllers/create.order.controllers.php
+++ controllers/create.order.controllers.php
@@ -3,6 +3,21 @@
 require_once("../config.php") ;
 
 
+// message au chargement de la page (base)
+$message = "";
 
-//ca fait appelle a la pages view si besoin 
+// condition qui vérifie l'envoi des valeurs dans le formulaire
+if (array_key_exists("quantity", $_POST) && 
+	array_key_exists("product", $_POST))
+{
+    // stockage des valeurs envoyées
+	$product = $_POST["product"];
+	$quantity = $_POST["quantity"];
+
+    // message à afficher avec les valeurs enregistrées
+	$message = "Votre commande contient : " .  $quantity . " " . $product . "!";
+}
+
+
+//ca fait appelle a la pages view (html) si besoin 
 require_once("../view/create.order.view.php");
--- partial/header.php
+++ partial/header.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
-</body>
-</html>
 
 <header>
 
-
-
-
-
+    <nav>
+        <ul>
+            <li><a href="#">Accueil de creations</a></li>
+            <li><a href="create-order-controller.php">Crée ta commande</a></li>
+        </ul>
+    </nav>
 
 </header>
+
+<main></main>
