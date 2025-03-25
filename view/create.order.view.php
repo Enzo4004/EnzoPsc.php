975f1a01984b8f06c82e92929bb5e1ce87be1b2d
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-03-25 14:49:47 +0100

    modification erreur

--- view/create.order.view.php
+++ view/create.order.view.php
@@ -1,10 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulaire</title>
+</head>
+<body>
+
+
 <!-- Importation du header -->
 <?php require_once('../partial/header.php'); ?>
 
 <section>
 
 <!-- Création formulaire -->
-    <form action="*">
+    <form method="GET">
 
     <!-- Champ texte pour mettre la quantité que l'on veut -->
         <label for="quantity">quantity
