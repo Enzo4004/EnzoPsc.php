9470f4eda235da30736637b4117b486a8bcfcc48
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-03-25 17:00:35 +0100

    initialisation de la session

--- controllers/create.order.controllers.php
+++ controllers/create.order.controllers.php
@@ -5,6 +5,31 @@ require_once("../config.php") ;
 //démarrage de la session
 session_start() ;
 
+// Vérification si le formulaire est soumis
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    // Récupération des données du formulaire
+    $quantity = isset($_POST['quantity']);
+    $product = isset($_POST['product']) ;
+
+    // Création du tableau $order
+    $order = [
+        'quantity' => $quantity,
+        'product' => $product,
+    ];
+
+    // Initialisation du panier dans la session s'il n'existe pas
+    if (!isset($_SESSION['orders'])) {
+        $_SESSION['orders'] = [];
+    }
+
+    // Ajout de la commande au panier
+    $_SESSION['orders'][] = $order;
+
+    echo "Commande ajoutée avec succès !";
+}
+
+
 // message au chargement de la page (base)
 $message = "";
 
