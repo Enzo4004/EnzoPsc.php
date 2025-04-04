d9dd86b5c9f84f98af8d7b4590af8b5e11bda0df
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-04-04 15:42:24 +0200

    css parce que c est cool

--- view/partial/header.php
+++ view/partial/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
@@ -14,6 +15,7 @@
         <ul>
             
             <li><a href="http://localhost/commerce.tshirt.php/controllers/create.order.controllers.php">Crée ta commande</a></li>
+            <li><a href="http://localhost/commerce.tshirt.php/controllers/pay.order.controllers.php">Afficher le paiement</a></li>
         </ul>
     </nav>
 
