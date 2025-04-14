856452cdab46be84cca55486382da9249420c091
Enzo Ramon <enzo.ramon@lapiscine.pro>
2025-04-14 12:24:26 +0200

    CORRECTION

--- model/order.model.php
+++ model/order.model.php
@@ -14,4 +14,27 @@ class Order {
 
     // Propriété qui représente le statut de la commande (ex : "PAID", "SHIPPED", CART , etc.)
     public $status;
+
+
+    public $status;
 }
+
+// je créé un objet, qui aura toutes les propriétés définies dans la classe.
+// pour l'instant ces propriétés sont vides, mais je peux les définir
+$order = new Order();
+
+// remplir les valeurs de chacune des propriétés
+$order->product = "Teeshirt Mario";
+$order->quantity = 1;
+$order->createdAt = new DateTime();
+$order->status = "Cart";
+
+
+
+
+
+
+
+
+
+
